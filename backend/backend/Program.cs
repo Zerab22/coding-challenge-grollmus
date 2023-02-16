90ff57d61024a7ece950b596b6b0a6be7259cbae
Ben <zerab.ben@gmail.com>
2023-02-16 15:38:17 +0100

    backend - allowed cors

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -5,21 +5,27 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DeviceDbContext>(options => options.UseInMemoryDatabase("Devices"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+var Origins = "_Origins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: Origins, policy => policy.WithOrigins("http://localhost:4200"));
+});
+
+/*builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
    .AddNegotiate();
 
 builder.Services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
-});
+});*/
 
 var app = builder.Build();
 
@@ -30,10 +36,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+app.UseCors(Origins);
 
 app.MapControllers();
 
