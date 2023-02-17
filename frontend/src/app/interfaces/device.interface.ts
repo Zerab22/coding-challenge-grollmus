1e74e00563a10b85c435ded9767feef19a10df45
Ben <zerab.ben@gmail.com>
2023-02-17 18:39:57 +0100

    frontend - code formatting

--- frontend/src/app/interfaces/device.interface.ts
+++ frontend/src/app/interfaces/device.interface.ts
@@ -1,17 +1,17 @@
 export interface Device {
-  id: string,
-  name: string,
-  deviceTypeId: string,
-  failsafe: boolean,
-  tempMin: number,
-  tempMax: number,
-  installationPosition: string,
-  insertInto19InchCabinet: boolean,
-  motionEnable: boolean,
-  siplusCatalog: boolean,
-  simaticCatalog: boolean,
-  rotationAxisNumber: number,
-  positionAxisNumber: number,
-  advancedEnvironmentalConditions?: boolean,
-  terminalElement?: boolean
+  id: string;
+  name: string;
+  deviceTypeId: string;
+  failsafe: boolean;
+  tempMin: number;
+  tempMax: number;
+  installationPosition: string;
+  insertInto19InchCabinet: boolean;
+  motionEnable: boolean;
+  siplusCatalog: boolean;
+  simaticCatalog: boolean;
+  rotationAxisNumber: number;
+  positionAxisNumber: number;
+  advancedEnvironmentalConditions?: boolean;
+  terminalElement?: boolean;
 }
